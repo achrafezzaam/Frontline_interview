9ea5647d5ed684755a6c453b1a6d3cd3206c6a31
Achraf Ezzaam <achraf.ezzaam@gmail.com>
2025-08-09 14:54:35 +0100

    feat: Adding the database handling functions
    Creating a base database handling. The main features implemented are:
    - Database initialization
    - Adding new items to the table
    - Updating items from the table by id
    - Searching items by id
    - Searching items by status
    - Listing all the items
    - Deleting items from table by id
    
    Future improvements to make to the model is to create a class
    where the function will be used as methods, this would result
    in a better code structure, reducing the redundant code,
    for example the constant need of creating and closing the connection
    to the database, the same for the cursor.

---
+++ main.py
@@ -0,0 +1,14 @@
+def scan(directory, dry_run):
+    return ""
+
+def purge(dry_run):
+    return ""
+
+def list():
+    return ""
+
+def restore(file_id):
+    return ""
+
+def delete(file_id):
+    return ""
---
+++ src/file_handler.py
@@ -0,0 +1,12 @@
+def find_inactive_files(directory, days_inactive):
+    return ""
+
+def archive_file(db_conn, file_path, archive_root_dir):
+    return ""
+
+def purge_archived_files(db_conn, days_archived):
+    return ""
+
+def restore_archived_file(db_conn, file_id):
+    return ""
+
